b15dc79e44ea231cecc074257092a487fb107317
Simon Lousky <simon@dagshub.com>
2021-07-09 10:41:47 +0300

    updated requirements, added the single head implementation from google/deeplab2

---
+++ src/models/heads.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+
+
+class PanopticDeepLabSingleHead(tf.keras.layers.Layer):
+    """A single PanopticDeepLab head layer.
+
+    This layer takes in the enriched features from a decoder and adds two
+    convolutions on top.
+    """
+
+    def __init__(self,
+                 intermediate_channels,
+                 output_channels,
+                 pred_key,
+                 name,
+                 conv_type='depthwise_separable_conv',
+                 bn_layer=tf.keras.layers.BatchNormalization):
+        """Initializes a single PanopticDeepLab head.
+
+        Args:
+          intermediate_channels: An integer specifying the number of filters of the
+            first 5x5 convolution.
+          output_channels: An integer specifying the number of filters of the second
+            1x1 convolution.
+          pred_key: A string specifying the key of the output dictionary.
+          name: A string specifying the name of this head.
+          conv_type: String, specifying head convolution type. Support
+            'depthwise_separable_conv' and 'standard_conv'.
+          bn_layer: An optional tf.keras.layers.Layer that computes the
+            normalization (default: tf.keras.layers.BatchNormalization).
+        """
+        super(PanopticDeepLabSingleHead, self).__init__(name=name)
+        self._pred_key = pred_key
+
+        self.conv_block = convolutions.StackedConv2DSame(
+            conv_type=conv_type,
+            num_layers=1,
+            output_channels=intermediate_channels,
+            kernel_size=5,
+            name='conv_block',
+            use_bias=False,
+            use_bn=True,
+            bn_layer=bn_layer,
+            activation='relu')
+        self.final_conv = layers.Conv2D(
+            output_channels,
+            kernel_size=1,
+            name='final_conv',
+            kernel_initializer=tf.keras.initializers.TruncatedNormal(stddev=0.01))
+
+    def call(self, features, training=False):
+        """Performs a forward pass.
+
+        Args:
+          features: A tf.Tensor with shape [batch, height, width, channels].
+          training: A boolean flag indicating whether training behavior should be
+            used (default: False).
+
+        Returns:
+          The dictionary containing the predictions under the specified key.
+        """
+        x = self.conv_block(features, training=training)
+        return {self._pred_key: self.final_conv(x)}
